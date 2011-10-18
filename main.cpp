0e592cb7a5fb1b7d65b8c33e24c4badf8786ef64
Rodrigo Setti <rodrigosetti@gmail.com>
2011-10-17 23:13:07 -0200

    using compile-time [ ] matching

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stack>
 #include <fstream>
 #include <iostream>
 
@@ -6,48 +7,26 @@
 
 using namespace std;
 
-int findOpening(const vector<char> &program, int pos) {
-    int stack = 0;
-
-    while (pos >= 0) {
-
-        if (program[pos] == ']') { stack ++; }
-        else if (program[pos] == '[') { 
-            stack --; 
-            if (stack == 0) { return pos; }
-        }
-
-        pos--;
+typedef struct {
+    char command;
+    union {
+        unsigned int match;
+        unsigned int repetition;
     }
-
-    return -1;
+    data;
 }
-
-int findClosing(const vector<char> &program, int pos) {
-    int stack = 0;
-
-    while (pos < program.size() ) {
-
-        if (program[pos] == '[') { stack ++; }
-        else if (program[pos] == ']') { 
-            stack --; 
-            if (stack == 0) { return pos; }
-        }
-
-        pos++;
-    }
-
-    return -1;
-}
+instruction_t;
 
 int main (int argc, char** argv) {
 
-    vector<char> program;
+    vector<instruction_t> program;
+    stack<unsigned int> loop_match;
     vector<int> memory(MEMORY_SIZE, 0);
     fstream file;
-    int pos = 0;
-    int process = 0;
-    char c, input;
+    instruction_t instruction;
+    unsigned int pos = 0;
+    unsigned int process = 0;
+    char c, last_c, input;
 
     if (argc >= 2) {
         file.open( argv[1], fstream::in);
@@ -57,53 +36,88 @@ int main (int argc, char** argv) {
         return 1;
     }
 
+    last_c = '\0';
+    process = 0;
     while (! file.eof() ) {
-        file >> c;
+        c = file.get();
         if (c == '<' || c == '>' || c == '+' || c == '-' || c == '.' || 
             c == ',' || c == '[' || c == ']') {
-            program.push_back( c );
+
+            if (c == last_c && c != '[' && c != ']') {
+                program.back().data.repetition ++;
+            }
+            else {
+                instruction.command = c;
+                if ( c == '[') {
+                    loop_match.push( process );
+                }
+                else if ( c == ']') {
+                    if (loop_match.empty()) {
+                        cerr << "Error: unmatched bracket ] (instruction #" << process << ')' << endl;
+                        file.close();
+                        return 1;
+                    }
+                    else {
+                        instruction.data.match = loop_match.top();
+                        program[ loop_match.top() ].data.match = process;
+                        loop_match.pop();
+                    }
+                }
+                else {
+                    instruction.data.repetition = 1;
+                }
+                program.push_back( instruction );
+                process ++;
+            }
+
+            last_c = c;
         }
     }
     file.close();
 
+    if (!loop_match.empty()) {
+        cerr << "unmatched bracket [ (instruction #" << loop_match.top() << ')' << endl;
+        return 1;
+    }
+
+    process = 0;
     while (process < program.size()) {
 
-        switch (program[process]) {
+        instruction = program[ process ];
+        switch (instruction.command) {
             case '>':
-                pos = (pos + 1) % MEMORY_SIZE;
+                pos = (pos + instruction.data.repetition) % MEMORY_SIZE;
                 break;
             case '<':
-                pos = (pos + (MEMORY_SIZE - 1)) % MEMORY_SIZE;
+                pos -= instruction.data.repetition;
+                while ( pos < 0) { pos += MEMORY_SIZE; }
                 break;
             case '+':
-                memory[pos] ++;
+                memory[pos] += instruction.data.repetition;
                 break;
             case '-':
-                memory[pos] --;
+                memory[pos] -= instruction.data.repetition;
                 break;
             case '[':
                 if (memory[pos] == 0) {
-                    process = findClosing( program, process);
-                    if (process == -1) {
-                        cerr << "unable to find closing ] at line " << process << endl;
-                        return 1;
-                    }
+                    process = instruction.data.match;
                 }
                 break;
             case ']':
                 if (memory[pos] != 0) {
-                    process = findOpening( program, process);
-                    if (process == -1) {
-                        cerr << "unable to find opening [ at line " << process << endl;
-                        return 1;
-                    }
+                    process = instruction.data.match;
                 }
                 break;
             case ',':
+                if (instruction.data.repetition > 1) {
+                    cin.ignore( instruction.data.repetition - 1 );
+                }
                 memory[pos] = cin.get();
                 break;
             case '.':
-                cout.put( memory[pos] );
+                for (unsigned int i=0; i < instruction.data.repetition; i++) {
+                    cout.put( memory[pos] );
+                }
                 break;
 
         }
